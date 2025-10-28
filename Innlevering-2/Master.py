2a8725a0fe165214e99719da51e6b60a375ed972
Kennymb <kennymb@uia.no>
2025-10-28 15:07:45 +0100

    Update Master.py

--- Innlevering-2/Master.py
+++ Innlevering-2/Master.py
@@ -1,1 +1,24 @@
+# **Gruppeinnlevering 1** #
+
+**Group number and student names:**
+Group 10
+
+Emma Moland Leiknes, emmaml@uia.no
+
+Kenny Minh Bui, Kennymb@uia.no 
+
+Lenia Barzan, Leniab@uia.no
+
+Lucas Fjeld, lucasf@uia.no 
+
+Sigve Sørensen, sigves@uia.no 
+
+Torbjørn Pedersen, torbjornp@uia.no
+
+Course Code & Name: IS-118 Programmering og prosjektsamarbeid
+
+Deliverable Title: Innleveringsoppgave 2
+
+Date: 28.Oktober 2025
+
 
