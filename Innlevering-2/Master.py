2ebd229798ad31cd1549496f39ea5a2e0d0a0950
leicnez <emmaml@uia.no>
2025-11-18 11:56:43 +0100

    Update Master.py

--- Innlevering-2/Master.py
+++ Innlevering-2/Master.py
@@ -28,4 +28,10 @@ ________________________________________________________________
 
 
 
+print("Velkommen til Erlings verden")
+print("Du har laget en medborgersportal og skal nå utvikle teamet ditt.")
+print("Det er flere konflikter som brygger og dine valg vil påvirke prosjektets suksess")
+print()
+print("Det er en grå morgen på kontoret. Humøret svinger og du er klar over hvordan teamet ikke er  på topp. I det du går inn døren til pause rommet hører du høylytte stemmer")
+print("Silje sier til sivert 'Du er så ubrukelig og du stinker'. Du ser Sivert gå ut av rommet litt lei seg")
 
